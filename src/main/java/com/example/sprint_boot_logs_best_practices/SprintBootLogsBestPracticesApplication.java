9a74f4a97b674b284d0b60c2fba006afe7edaece
Julian Henao Marin <julianhm9612@gmail.com>
2024-07-05 10:08:10 -0500

    Initial code

---
+++ src/main/java/com/example/sprint_boot_logs_best_practices/SprintBootLogsBestPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprint_boot_logs_best_practices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootLogsBestPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootLogsBestPracticesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprint_boot_logs_best_practices/SprintBootLogsBestPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprint_boot_logs_best_practices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootLogsBestPracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
